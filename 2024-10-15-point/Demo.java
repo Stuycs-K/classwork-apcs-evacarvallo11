d65dc8f4c71cf7a7618c8f02ec8d0983b8da664d
Eva Carvallo <ecarvallo60@stuy.edu>
2024-10-16 00:04:12 -0400

    added on my computer

---
+++ 2024-10-15-point/Demo.java
@@ -0,0 +1,28 @@
+public class Demo{
+
+  /*
+  Write this method third. Test it then move on
+  to the others.
+  */
+  public static double distance(Point a, Point b){
+    double xsub = b.getX() - a.getX();
+    double ysub = b.getY() - a.getY();
+    return Math.sqrt((xsub * xsub) + (ysub * ysub));
+  }
+
+  public static void main(String[]args){
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(-1,-1);
+    Point p3 = new Point(3,4);
+    Point p4 = new Point(p3);
+    Point triangle1 = new Point(0,0);
+    Point triangle2 = new Point(1,0);
+    Point triangle3 = new Point(1/2,Math.sqrt(3) / 2);
+
+    System.out.println( p3);
+    System.out.println( distance(p1,p2));
+    System.out.println( Point.distance(p1,p2));
+    System.out.println( p1.distanceTo(p2));
+    System.out.println(p4);
+  }
+}
---
+++ 2024-10-15-point/Point.java
@@ -0,0 +1,46 @@
+public class Point{
+  private double x,y;
+
+  public String toString(){
+    return "(" + x + "," + y + ")";
+  }
+
+  //Initialize this Point to have the same values as the other Point.
+  public Point(Point other){
+    //COMPLETE THIS SECOND
+    //Then write test cases.
+    x = other.getX();
+    y = other.getY();
+  }
+
+  //Initialize this Point to have the provided values
+  public Point(double x, double y){
+    this.x=x;
+    this.y=y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+
+  /*
+  Write this method FOURTH, go to the main and do that one first.
+  */
+  public double distanceTo(Point other){
+    double xsub = other.getX() - this.getX();
+    double ysub = other.getY() - this.getY();
+    return Math.sqrt((xsub * xsub) + (ysub * ysub));
+  }
+
+  /*
+  Write this method last. Re-use (NOT COPY/PASTE) prior work and
+  do not write redundant code.
+  */
+  public static double distance(Point a, Point b){
+    return Demo.distance(a,b);
+  }
+
+
+}
