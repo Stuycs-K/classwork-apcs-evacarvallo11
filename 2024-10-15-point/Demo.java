b8e72bc3053a81ca80472e91fa13adc3c7a312b8
Eva Carvallo <ecarvallo60@stuy.edu>
2024-10-16 00:10:10 -0400

    find distances of equilateral triangle

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -17,12 +17,19 @@ public class Demo{
     Point p4 = new Point(p3);
     Point triangle1 = new Point(0,0);
     Point triangle2 = new Point(1,0);
-    Point triangle3 = new Point(1/2,Math.sqrt(3) / 2);
+    Point triangle3 = new Point(1.0/2,Math.sqrt(3) / 2);
 
     System.out.println( p3);
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
     System.out.println(p4);
+    System.out.println(triangle1);
+    System.out.println(triangle2);
+    System.out.println(triangle3);
+    System.out.println(distance(triangle1, triangle2));
+    System.out.println(distance(triangle2, triangle3));
+    System.out.println(distance(triangle1, triangle3));
+
   }
 }
