62f08a4f150e38dd705f222f022f037df116d958
Eva Carvallo <ecarvallo60@stuy.edu>
2024-11-08 06:50:29 -0500

    comments

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,6 +1,8 @@
 //It changed the String that getName() returned by attaching "The Mighty" to the front
 //Since getName is overwritten, speak() now uses the new name
 
+//an animal class could not be a bird because Bird is an extension of the animal class.
+//Animal is the overarching class that gets more specific to become a bird. Thus, if we were to write another subclass for Animal, you couldn't make it a bird and then generalize it again.
 
 public class Driver{
 
@@ -9,6 +11,10 @@ public class Driver{
     a1.speak();
     Bird b1 = new Bird("Squakkkkkkk", 7, "Fido", 5.0, "yellow");
     b1.speak();
+    //Bird b2 = new Animal("Squack", 3, "bluejay");
+    Animal a2 = new Bird("Squakkkkkkk", 7, "Fido", 5.0, "yellow");
+    //b2.speak();
+    a2.speak();
   }
 
 }
