5c7cd225f53500b1b41507b34dad11eee20d1abb
Eva Carvallo <ecarvallo60@stuy.edu>
2024-11-20 10:00:04 -0600

    working on solving

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -1,26 +1,45 @@
 import java.util.*;
 import java.io.*;
 class Day1 {
-    public static[] parse(String filename)
+    public static String[] parse(String filename)
     {
-      Scanner inf = new Scanner(File(filename));
+      try{
+      Scanner inf = new Scanner(new File(filename));
       String line = inf.nextLine();
       while (inf.hasNextLine())
       {
         line+= inf.nextLine();
       }
-      return inf.split(",");
-    }
-    public static void main(String[] args) {
-      try {
-      File file = new File("input.java");
-      System.out.println(parse(file).toString());
-
-
+      return line.split(",");
     } catch (FileNotFoundException ex) {
       //File not found what should you do?
       System.out.println("File not found");
-      return; //you can return from a void function just don't put a value.
+      return null; //you can return from a void function just don't put a value.
     }
+    }
+
+  public static String aryToString(String[] nums) {
+    String fin = "[";
+    for (int i = 0; i < nums.length; i++) {
+      fin += nums[i];
+      if (i < nums.length - 1) {
+        fin += ", ";
+      }
+    }
+    return fin + "]";
+  }
+
+  public static int solve(String[] data)
+  {
+    for (int i = 0; i < data.length; i++)
+    {
+      
+    }
+  }
+
+    public static void main(String[] args) {
+
+      System.out.println(aryToString(parse("input.txt")));
+
     }
   }
