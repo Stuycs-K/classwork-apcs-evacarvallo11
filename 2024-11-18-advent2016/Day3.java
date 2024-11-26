d49e4d8d45ec1e65ed190e208a253fbdd22a104c
Eva Carvallo <158617101+evacarvallo11@users.noreply.github.com>
2024-11-26 08:18:14 -0600

    fixed debugging. couldn't push before

--- 2024-11-18-advent2016/Day3.java
+++ 2024-11-18-advent2016/Day3.java
@@ -10,23 +10,21 @@ public static int countTrianglesA(String filename)
   Scanner input = new Scanner(file);
 
   int count = 0;
+  String line;
   while (input.hasNextLine())
   {
-    if (input.hasNextInt()) {
-      int x = input.nextInt();
-      if (input.hasNextInt()) {
-        int y = input.nextInt();
-        if (input.hasNextInt()) {
-          int z = input.nextInt();
-          if (ifTriangle(x, y, z)) {
-            count++;
-              }
-          }
-        }
+    line = input.nextLine();
+    String[] parts = line.split("\\s+");
+
+    int a = Integer.parseInt(parts[1]);
+    int b = Integer.parseInt(parts[2]);
+    int c = Integer.parseInt(parts[3]);
+    if (ifTriangle(a, b, c))
+    {
+      count++;
+    }
   }
-  }
-  input.close();
-    return count;
+  return count;
   }catch(FileNotFoundException ex) {
       System.out.println("File not found");
       return 0;
@@ -37,20 +35,26 @@ public static boolean ifTriangle(int x, int y, int z)
 {
   return x + y > z && x + z > y && y + z > x;
 }
-/*
+
+
 public static int countTrianglesB(String filename)
 {
-    File file = new File("inputTri.text");//1
+  try{
+    File file = new File(filename);//1
       Scanner input = new Scanner(file);
  int count = 0;
    ArrayList<Integer>row1 = new ArrayList<Integer>();
    ArrayList<Integer>row2 = new ArrayList<Integer>();
    ArrayList<Integer>row3 = new ArrayList<Integer>();
+     String line;
     while(input.hasNextInt())
    {
-     int x = input.nextint();
-     int y = input.nextint();
-     int z = input.nextint();
+     line = input.nextLine();
+    String[] parts = line.split("\\s+");
+
+    int x = Integer.parseInt(parts[1]);
+    int y = Integer.parseInt(parts[2]);
+    int z = Integer.parseInt(parts[3]);
    row1.add(x);
    row2.add(y);
    row3.add(z);
@@ -77,11 +81,44 @@ public static int countTrianglesB(String filename)
    }
  }
  return count;
+}catch(FileNotFoundException ex) {
+      System.out.println("File not found");
+      return 0;
+    }
+}
+
+public static String aryToString(String[] nums) {
+    String fin = "[";
+    for (int i = 0; i < nums.length; i++) {
+      fin += nums[i];
+      if (i < nums.length - 1) {
+        fin += ", ";
+      }
+    }
+    return fin + "]";
+  }
+
+  public static void main(String[] args) {
+    System.out.println(countTrianglesA("input3.txt"));
+    System.out.println(countTrianglesB("input3.txt"));
+    /*
+    try{
+  File file = new File("input3.txt");
+  Scanner input = new Scanner(file);
+
+  int count = 0;
+  String line;
+  while (input.hasNextLine())
+  {
+    line = input.nextLine();
+    String[] parts = line.split("\\\s+");
+    System.out.println(parts.length);
+    System.out.println(aryToString(parts));
+    }
+    }catch(FileNotFoundException ex) {
+        System.out.println("File not found");
+      }
 }
 */
-  public static void main(String[] args) {
-
-    System.out.println(countTrianglesA("input4.txt"));
-
-    }
+}
 }
