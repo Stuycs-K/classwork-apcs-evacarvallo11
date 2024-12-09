9902927f85b88ca424ba05365c5e4525d7f3067a
Eva Carvallo <158617101+evacarvallo11@users.noreply.github.com>
2024-12-08 23:21:06 -0500

    Update Driver.java

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,3 +1,10 @@
 public class Driver{
-
+  public static void main(String[] args)
+  {
+    Adventurer x = new Warrior("Xavier", 40);
+    Adventurer y = new Warrior("Justin", 40);
+    System.out.println(y.support(x));
+    System.out.println(y.support());
+    System.out.println(y.specialAttack(x));
+  }
 }
