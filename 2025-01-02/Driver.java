6e6c8ca447e27f890c4e96dff052e344cca628c7
Eva Carvallo <ecarvallo60@stuy.edu>
2025-01-03 10:00:11 -0600

    make words

--- 2025-01-02/Driver.java
+++ 2025-01-02/Driver.java
@@ -1,7 +1,10 @@
 public class Driver{
+
     public static void main(String[] args)
   {
-    System.out.println(Fib.fb(60));
+    //System.out.println(Fib.fb(60));
+    Words.makeWords(3, "", "abc");
   }
   }
-//46 is the largest int for under 10 sec
+
+//46 is the largest int for under 10 sec and largest before overflow
